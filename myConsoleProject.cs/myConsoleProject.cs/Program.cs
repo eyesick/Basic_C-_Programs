67b5e3703835e2dd94bee0c61116f9a03cc29e05
Isaac Suehiro <isaac.suehiro@gmail.com>
2020-10-20 10:16:37 -1000

    Initial commit

---
+++ myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, We're \"Accounting\" on you" );
+            Console.Read();
+        }
+    }
+}
