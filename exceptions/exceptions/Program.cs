2897d19c4ed1473f4b2cc5802fb579a6f02cdc0f
Isaac Suehiro <isaac.suehiro@gmail.com>
2020-10-26 08:37:08 -1000

    trycatch

---
+++ exceptions/exceptions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+        List<int> numbers = new List<int> {2,5,9,14,32,77,84 };
+        try
+        {
+            Console.WriteLine("enter a positive whole number");
+            int input = Convert.ToInt32(Console.ReadLine());
+            foreach (int number in numbers)
+            {
+                double newnumber = number / input;
+                Console.WriteLine(newnumber);
+            }
+        }
+        catch(System.DivideByZeroException ex)
+        {
+            Console.WriteLine("you cannot divide by zero");
+        }
+        catch(System.FormatException ex)
+        {
+            Console.WriteLine("You have entered a non number");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("The program will continue");
+            Console.ReadLine();
+        }
+        }
+
+ }
+    
+
