d6da432e164235a998b01b59f49160f572f004ed
Isaac Suehiro <isaac.suehiro@gmail.com>
2020-10-26 09:51:21 -1000

    21

---
+++ TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            deck = Shuffle(deck);
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
+        }
+   
+
+    public static Deck Shuffle(Deck deck)
+    {
+        Random random = new Random();
+        List<Card> TempList = new List<Card>();
+        
+        while (deck.Cards.Count > 0)
+        {
+            int randomIndex = random.Next(0, deck.Cards.Count);
+            TempList.Add(deck.Cards[randomIndex]);
+            deck.Cards.RemoveAt(randomIndex);
+
+        }
+        deck.Cards = TempList;
+        return deck;
+        }
+    }
+}
