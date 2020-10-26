ba6dbf0c2f6bbe75602b0c9822a514bb81500820
Isaac Suehiro <isaac.suehiro@gmail.com>
2020-10-25 19:57:12 -1000

    strings

---
+++ strings/strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Hello what is your name?");
+        string name = Console.ReadLine();
+        string uppername = name.ToUpper();
+        Console.WriteLine("How old are you?");
+        string age = Console.ReadLine();
+        Console.WriteLine("what is your favorite food?");
+        string food = Console.ReadLine();
+        string userinputs = name + "\n" + age + "\n" + food; 
+        StringBuilder sb = new StringBuilder();
+        Console.WriteLine("you gave the answers\n" + userinputs);
+        sb.Append("Hello " + uppername);
+        sb.Append(" You are " + age + " years old");
+        sb.Append(" your favorite food is " + food);
+        Console.WriteLine(sb);
+        Console.ReadLine();
+        }
+    }
+
