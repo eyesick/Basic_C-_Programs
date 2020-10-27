5ff22d58bc528e8eb68abccbbf09f1c093a73864
Isaac Suehiro <isaac.suehiro@gmail.com>
2020-10-27 03:24:40 -1000

    method overload

---
+++ methodovr/methodovr/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodovr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please provide a number");
+            int input = Convert.ToInt32(Console.ReadLine());
+            math Math = new math();
+            int x2 = Math.by2(input);
+            Console.WriteLine(x2);
+            int secondx2 = Math.by2(input, 3.3m);
+            int thirdx2 = Math.by2(x2, secondx2, "product");
+            Console.ReadLine();        }
+    }
+}
---
+++ methodovr/methodovr/math.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodovr
+{
+    public class math
+    {
+        public int by2(int a)
+        {
+            int result = a * 2;
+            return result;
+
+        }
+
+        public int by2(int a, decimal b)
+        {
+            decimal result = a * 2 * b;
+            Convert.ToInt32(result);
+            return (int)result;
+
+        }
+
+        public int by2(int a, int b, string word)
+        {
+            Convert.ToInt32(b);
+            int result = a * b;
+            Convert.ToString(result);
+            Console.WriteLine(result + word);
+            return result;
+
+
+        }
+    }
+}
