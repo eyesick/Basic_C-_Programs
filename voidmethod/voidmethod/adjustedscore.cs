284fc74e1185fa795832f07e0b9cf7dec934a35e
Isaac Suehiro <isaac.suehiro@gmail.com>
2020-10-27 04:12:49 -1000

    named perameter

---
+++ voidmethod/voidmethod/adjustedscore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voidmethod
+{
+    class adjustedscore
+    {
+        public static void Adjustedscore(int a, int b)
+        {
+
+        }
+    }
+}
