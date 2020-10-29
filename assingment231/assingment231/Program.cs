25163d2912f60433c5ff5607ecd1d83b990bf1fa
Isaac Suehiro <isaac.suehiro@gmail.com>
2020-10-29 08:25:55 -1000

    231

---
+++ assingment231/assingment231/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assingment231
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("inpput a number to be divided by 2");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            method.Voidmethod(input, out result);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assingment231/assingment231/method.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assingment231
+{
+    static class method
+    {
+        public static void Voidmethod(int a, out int result)
+        {
+            result = a / 2;
+
+
+        }
+
+        public static void Voidmethod(int speed, int weight, out int force, out int stoptime)
+        {
+            force = speed * weight;
+            stoptime = speed / force;
+        }
+
+        public static void Consoleout()
+        {
+            Console.WriteLine("This is a static method");
+        }
+    }
+}
