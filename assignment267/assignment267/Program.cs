92fa5bf50369d10ae7a7ddfd7a84ad3f99028d4d
Isaac Suehiro <isaac.suehiro@gmail.com>
2020-11-02 06:31:13 -1000

    267

---
+++ assignment267/assignment267/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment267
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 10m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+
+        public struct Number
+        {
+            public decimal Amount;
+        }
+    }
+}
