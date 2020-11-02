b16b742d467375b030e4a6781e25fbf29974a260
Isaac Suehiro <isaac.suehiro@gmail.com>
2020-11-02 03:23:33 -1000

    244

---
+++ assignment244/assignment244/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment244
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee test = new Employee();
+            test.firstName = "Slim";
+            test.lastName = "Shadey";
+            test.SayName();
+            Console.WriteLine(test.firstName + " " + test.lastName);
+            Console.ReadLine();
+
+        }
+    }
+}
