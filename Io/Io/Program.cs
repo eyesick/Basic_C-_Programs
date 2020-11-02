7c8547e6b766af366fd424b8ca57bff700319c05
Isaac Suehiro <isaac.suehiro@gmail.com>
2020-11-02 10:33:04 -1000

    297

--- Io/Io/Program.cs
+++ Io/Io/Program.cs
@@ -12,20 +12,29 @@ namespace Io
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a number");
-            string text = Console.ReadLine();
-            
-            using (StreamWriter file = new StreamWriter(@"C:\Users\isaac\Documents\logs.txt", true))
-            {
-                file.WriteLine(text);
-                
-            }
-            string[] lines = File.ReadAllLines(@"C:\Users\isaac\Documents\logs.txt");
-            foreach (string line in lines)
-            {
-                Console.WriteLine(line);
-            }
+            //Console.WriteLine("Please enter a number");
+            //string text = Console.ReadLine();
+
+            //using (StreamWriter file = new StreamWriter(@"C:\Users\isaac\Documents\logs.txt", true))
+            //{
+            //    file.WriteLine(text);
+
+            //}
+            //string[] lines = File.ReadAllLines(@"C:\Users\isaac\Documents\logs.txt");
+            //foreach (string line in lines)
+            //{
+            //    Console.WriteLine(line);
+            //}
+            //Console.ReadLine();
+
+            DateTime timenow = DateTime.Now;
+            Console.WriteLine(timenow);
+            Console.WriteLine("Please entere number");
+            int x = Convert.ToInt32(Console.ReadLine());     
+            Console.WriteLine(timenow.AddHours(x));
             Console.ReadLine();
+
+
         }
     }
 }
