7b1da2a1d1ea4c5a97ab728acb0264eb1a54e95e
Isaac Suehiro <isaac.suehiro@gmail.com>
2020-11-08 13:44:57 -1000

    451

---
+++ WebApplication3/WebApplication3/Models/SchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using WebApplication3.Models;
+
+namespace EF6Console
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
