e22381725131efb3cee76845f10bc4847382939e
Isaac Suehiro <isaac.suehiro@gmail.com>
2020-11-17 19:34:28 -0700

    158

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -10,6 +10,21 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please input a number");
+            long input = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine(input * 50);
+            Console.WriteLine("Please input a number");
+            long input2 = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine(input2 + 25);
+            Console.WriteLine("Please input a number");
+            long input3 = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine(input3 / 12.5);
+            Console.WriteLine("Please input a number");
+            long input4 = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine(input4 > 50);
+            Console.WriteLine("Please input a number");
+            long input5 = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine(input5 % 7);
         }
     }
 }
